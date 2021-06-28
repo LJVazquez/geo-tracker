f46d860a70d1375b0407748962e926a76160add8
Ljvazquez <ljvazquez00@gmail.com>
2021-06-28 18:38:20 -0300

    agregado marcadores de posicion actual

--- laravel/resources/views/map.blade.php
+++ laravel/resources/views/map.blade.php
@@ -8,14 +8,41 @@
     <title>Document</title>
 </head>
 <style>
+    * {
+        margin: 0;
+        padding: 0
+    }
+
     #map {
         height: 100vh;
         width: 100%;
     }
 
+    #coords {
+        position: absolute;
+        top: 10px;
+        left: 10px;
+        z-index: 1;
+        padding: 1rem;
+        background-color: rgba(61, 64, 67, 0.5);
+        border-radius: 5px;
+        color: #FFF;
+        font-family: Segoe UI, Frutiger, Frutiger Linotype, Dejavu Sans, Helvetica Neue, Arial, sans-serif;
+    }
+
+    #coords h1 {
+        font-size: 1.5rem;
+        margin: 0;
+    }
+
 </style>
 
 <body>
+    <div id="coords">
+        <h1>Posicion actual</h1>
+        <p>Latitud: <span id="lat">0</span></p>
+        <p>Longitud: <span id="lng">0</span></p>
+    </div>
     <div id="map"></div>
     <script src="{{ asset('js/map.js') }}"></script>
     <script src="https://maps.googleapis.com/maps/api/js?key={{ env('GMAPS_KEY') }}&callback=initMap&libraries=&v=weekly"
